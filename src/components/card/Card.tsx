396fee5c0b63b4593414ee6d4261c7358fae762f
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-22 15:02:42 +0545

    creating card to wrap card data

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,16 @@
+const Card = () => {
+  return (
+    <div className="bg-[#364152] rounded p-4">
+      <h1 className="text-gray-300 font-semibold">Your Details :</h1>
+      {/* 
+      <div className="flex justify-between flex-wrap  text-sm text-gray-300 mt-5">
+        <p>Name: Abhishek Shrestha</p>
+        <p>Email: AbhishekShrestha@gmail.com</p>
+        <p>Class: 10</p>
+        <p>Total Attendance: 100</p>
+      </div> */}
+    </div>
+  );
+};
+
+export default Card;
