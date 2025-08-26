30a8fc2ad039f60f3b26945c2ebb6b678d72f5a0
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-26 20:53:34 +0545

    sign in registration

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,42 +1,93 @@
-// import Input from "../shared/DesignSystem/input/Input";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface ILoginData {
+  email: string;
+  password: string;
+}
+
+const signInSchema = yup.object({
+  email: yup.string().email().required("email is required"),
+  password: yup.string().required("password is required"),
+});
 
 const Signin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(signInSchema),
+    mode: "all",
+  });
+
+  const onSubmit = (data: ILoginData) => {
+    console.log(data);
+  };
+
+  // console.log(watch);
   return (
     <div className="h-full w-screen flex  justify-center items-center bg-gray-300">
-      <div className="p-6 min-h-[400px] min-w-[500px] bg-white rounded shadow-2xl flex flex-col gap-6">
+      <div className="p-6 min-h-[400px] min-w-[500px] bg-white rounded shadow-2xl">
+        {/* heading signin */}
+
         <h1 className="text-center text-2xl font-semibold text-indigo-600 ">
           Login
         </h1>
 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="text-lg font-semibold">
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="example@gmail.com"
-            className="px-2 py-2 outline-none border-2 border-indigo-400 rounded  focus:border-indigo-800"
-          />
-        </div>
+        {/* email input  */}
 
-        <div className="flex flex-col">
-          <label htmlFor="password" className="text-lg font-semibold">
-            Password
-          </label>
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="password"
-            className="px-2 py-2 outline-none border-2 border-indigo-400 rounded focus:border-indigo-800"
-          />
-        </div>
-        {/* <Input variant="default" type="text" label="Test" id="test-id" /> */}
-        <button className="w-full mt-2 py-2 bg-indigo-600 rounded cursor-pointer font-semibold text-md text-gray-300 hover:bg-indigo-800 transition-all duration-500">
-          Login
-        </button>
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-lg font-semibold">
+              Email
+            </label>
+            <input
+              type="text"
+              // name="email"
+              id="email"
+              placeholder="example@gmail.com"
+              {...register("email")}
+              className="px-2 py-2 outline-none border-2 border-indigo-400 rounded  focus:border-indigo-800"
+            />
+            {errors.email ? (
+              <p className="text-red-500 text-sm">
+                {errors.email.message as string}
+              </p>
+            ) : (
+              <p className="text-transparent text-sm"> </p>
+            )}
+          </div>
+
+          {/* password input */}
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-lg font-semibold">
+              Password
+            </label>
+            <input
+              type="password"
+              // name="password"
+              id="password"
+              placeholder="password"
+              {...register("password")}
+              className="px-2 py-2 outline-none border-2 border-indigo-400 rounded focus:border-indigo-800"
+            />
+            {errors.password ? (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            ) : (
+              <p className="text-transparent text-sm"> </p>
+            )}
+          </div>
+
+          <button className="w-full mt-2 py-2 bg-indigo-600 rounded cursor-pointer font-semibold text-md text-gray-300 hover:bg-indigo-800 transition-all duration-500">
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
