b88850cd7976945d160fb0eef2714344476fb936
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-28 12:29:12 +0545

    isolate file for studentdata

---
+++ src/components/userRegistration/StudentData.tsx
@@ -0,0 +1,27 @@
+import Input from "../../shared/DesignSystem/input/Input";
+import DropDown from "../../shared/DesignSystem/dropdown/DropDown";
+import { classesOption } from "../../types/global.type";
+import type { FieldValues, UseFormRegister } from "react-hook-form";
+
+export const StudentData = ({
+  register,
+}: {
+  register: UseFormRegister<FieldValues>;
+}) => {
+  return (
+    <>
+      <div className="w-full md:w-[32%]">
+        <Input
+          id="rollNumber"
+          type="text"
+          placeholder="Roll number"
+          label="Roll Number"
+          register={register}
+        />
+      </div>
+      <div className="w-full md:w-[32%]">
+        <DropDown dropdownOptions={classesOption} title="Class" id="Class" />
+      </div>
+    </>
+  );
+};
