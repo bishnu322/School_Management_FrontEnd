58495086e8e6a9f000ba9dfb59e433cf480498c5
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-28 10:45:48 +0545

    resposive

--- src/components/sideNav/SideNav.tsx
+++ src/components/sideNav/SideNav.tsx
@@ -1,5 +1,4 @@
 import {
-  RiMenuFill,
   RiDashboardHorizontalFill,
   RiDashboard3Line,
   RiUser3Fill,
@@ -21,28 +20,21 @@ const sideNavConfig = {
   ],
 };
 
-interface SideNavProps {
-  isOpen: boolean;
-  toggle: () => void;
-}
-
-const SideNav = ({ isOpen, toggle }: SideNavProps) => {
+const SideNav = () => {
   return (
     <div
-      className={`fixed top-0 left-0 h-full z-50 bg-[#0A2335] text-gray-200 transition-transform duration-300
-        ${isOpen ? "translate-x-0" : "-translate-x-full"} 
-        sm:translate-x-0 sm:w-[250px] sm:static`}
+      className={`h-full  bg-[#0A2335] text-gray-200 transition-transform duration-300 w-[250px]`}
     >
       <div className="flex justify-between items-center p-5 text-2xl font-semibold">
         <div className="flex gap-2 items-center">
           <RiDashboardHorizontalFill />
           DashBoard
         </div>
-        <RiMenuFill
+        {/* <RiMenuFill
           size={25}
           className="sm:hidden cursor-pointer"
           onClick={toggle}
-        />
+        /> */}
       </div>
 
       <div className="mt-4">
@@ -51,7 +43,7 @@ const SideNav = ({ isOpen, toggle }: SideNavProps) => {
             key={link}
             to={to}
             className="flex gap-2 py-3 px-5 font-semibold hover:bg-[#4d5c67] hover:text-gray-300"
-            onClick={() => isOpen && toggle()} // close on mobile click
+            // close on mobile click
           >
             <Icon />
             <div>{link}</div>
