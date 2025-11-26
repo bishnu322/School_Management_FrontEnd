d33fb41662945262c5bd0a80dece2e55c0a6de63
bishnu_kohar <koharbishnu322@gmail.com>
2025-11-26 15:19:47 +0545

    fetching data of student with their attendance

--- src/api/attendance.ts
+++ src/api/attendance.ts
@@ -1,9 +1,7 @@
 import { api } from "./Axios/axiosInstance";
 
 export const getAttendanceByUserId = async (user_id: string) => {
-  const response = await api.get(`/attendance`, {
-    params: { user_id },
-  });
+  const response = await api.get(`/attendance/${user_id}`);
 
   console.log(response.data);
 
--- src/components/attendance/UpdateAttendance.tsx
+++ src/components/attendance/UpdateAttendance.tsx
@@ -1,12 +1,9 @@
 // // import { useQuery } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
 import { getStudentByIdApi } from "../../api/student/studentApi";
-import DatePicker from "./DatePicker";
-import { AttendanceSelector } from "./table/AttendanceSelector";
 import CloseIcon from "@mui/icons-material/Close";
 import Button from "@mui/material/Button";
-import { useMemo, useState } from "react";
-import { getAttendanceByUserId } from "../../api/attendance";
+import { useState } from "react";
 
 interface IProps {
   name?: string;
@@ -14,60 +11,52 @@ interface IProps {
   user_id: string;
 }
 
-const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  }).format(date);
-};
+// const formatDate = (dateString: string) => {
+//   const date = new Date(dateString);
+//   return new Intl.DateTimeFormat("en-US", {
+//     year: "numeric",
+//     month: "short",
+//     day: "numeric",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   }).format(date);
+// };
 
 const UpdateAttendance: React.FC<IProps> = ({
   // name = "default",
   setUpdateAttendanceSection,
   user_id,
 }) => {
-  const [userStatus, setUserStatus] = useState("");
-
   // fetching individual student data
   const { data, isLoading } = useQuery({
-    queryKey: ["getAttendanceByUserId"],
-    queryFn: () => getAttendanceByUserId(user_id),
+    queryKey: ["getStudentByIdApi"],
+    queryFn: () => getStudentByIdApi(user_id),
   });
 
   // memoizing created and updated data of the student
-  const formattedDates = useMemo(() => {
-    if (!data?.data.user_id) return { date: "" };
+  // const formattedDates = useMemo(() => {
+  //   if (!data?.data.user_id) return { date: "" };
 
-    return {
-      createdAt: formatDate(data.data.user_id.date),
-      // updatedAt: formatDate(data.data.user_id.updatedAt),
-    };
-  }, [data?.data.user_id]);
+  //   return {
+  //     date: formatDate(data.data.user_id.date),
+  //   };
+  // }, [data?.data.user_id]);
 
-  // until the data is loaded...
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>loading..</div>;
 
-  // fetching the attendance status from the child component
-  const upsertAttendanceHandler = (status: string) => {
-    setUserStatus(status);
-  };
-
-  console.log("Attendance Data", data);
+  console.log("Attendance Data", data.data.user.first_name);
 
   return (
     <main
       className="bg-gray-300/50 backdrop-blur-[1px] absolute inset-0 h-full w-full 
                     flex justify-center items-center "
     >
-      <div className="min-h-1/4 w-md bg-gray-600 p-5 rounded flex flex-col gap-4 text-gray-300">
+      <div className="min-h-1/4 w-lg bg-gray-600 p-5 rounded flex flex-col gap-4 text-gray-300">
         <div className="flex justify-between items-center">
-          {/* selecting data from date picker component */}
-          {/* <DatePicker /> */}
-          <div>User Attendance Details</div>
+          {/* header of the update section */}
+          <div className="text-gray-300 font-semibold border  px-2 py-1 rounded bg-gray-700">
+            User Attendance Details
+          </div>
 
           {/* this icon is used to close the opened update component */}
           <CloseIcon
@@ -75,34 +64,47 @@ const UpdateAttendance: React.FC<IProps> = ({
             onClick={() => setUpdateAttendanceSection(false)}
           />
         </div>
-        {/* 
-        <div className="flex justify-between">
-          <div>
-            <span className="text-gray-400">Name: </span>
-            {data?.data.user_id.first_name ?? ""}{" "}
-            {data?.data.user_id.last_name ?? " "}
-          </div> */}
 
-        {/* select the status of the attendance */}
+        {/* main body of update section */}
         <div>
-          <span className="text-gray-400">Status: </span>
-          <AttendanceSelector
-            user_id={user_id}
-            upsertAttendanceHandler={upsertAttendanceHandler}
-          />
-        </div>
-        {/* </div> */}
+          <div>
+            <span>Name: </span>
 
-        {/* <div>
-          <div>
-            <span className="text-gray-400">Created At: </span>
-            {formattedDates.createdAt ?? "N/A"}
+            {data.data?.user?.first_name && data.data?.user?.last_name
+              ? `${data.data.user.first_name} ${data.data.user.last_name}`
+              : "N/A"}
           </div>
-          <div>
-            <span className="text-gray-400">Updated At: </span>
-            {formattedDates.updatedAt ?? "N/A"}
-          </div>
-        </div> */}
+
+          <table className="min-w-full rounded bg-[#364152] text-gray-300 text-left mt-1">
+            <thead className="bg-gray-900 text-md text-gray-400 text-center">
+              <tr>
+                <th className="border-r border-gray-500 px-4 py-2">Date</th>
+                <th className="border-r border-gray-500 px-4 py-2">Status</th>
+                <th className="border-r border-gray-500 px-4 py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.data.attendance
+                ? data?.data.attendance.map((attendance, index) => (
+                    <tr
+                      className="hover:bg-gray-600 text-sm border-b border-gray-600"
+                      key={index}
+                    >
+                      <td className="border-r border-gray-600 px-4 py-2">
+                        {attendance.date}
+                      </td>
+                      <td className="border-r border-gray-600 px-4 py-2">
+                        {attendance.status}
+                      </td>
+                      <td className="border-r border-gray-600 px-4 py-2">
+                        fej
+                      </td>
+                    </tr>
+                  ))
+                : ""}
+            </tbody>
+          </table>
+        </div>
 
         {/* update button of attendance */}
         <Button variant="contained">Update</Button>
